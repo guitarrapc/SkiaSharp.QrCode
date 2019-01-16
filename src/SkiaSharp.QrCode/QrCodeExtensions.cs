967f8d66c01980fd9f6b7248acbe39b42dfeedd2
Ikiru Yoshizaki <guitarrapc@users.noreply.github.com>
2019-01-17 00:46:02 +0900

    fear: add additional override Render extension method

---
+++ src/SkiaSharp.QrCode/QrCodeExtensions.cs
@@ -0,0 +1,39 @@
+﻿using SkiaSharp;
+
+namespace SkiaQrCode
+{
+    public static class QrCodeExtensions
+    {
+        public static void Render(this SKCanvas canvas, QRCodeData data, int width, int hight)
+        {
+            canvas.Clear(SKColors.Transparent);
+
+            using (var renderer = new QRCodeRenderer())
+            {
+                var area = SKRect.Create(0, 0, width, hight);
+                renderer.Render(canvas, area, data);
+            }
+        }
+
+        public static void Render(this SKCanvas canvas, QRCodeData data, int width, int hight, SKColor clearColor)
+        {
+            canvas.Clear(clearColor);
+
+            using (var renderer = new QRCodeRenderer())
+            {
+                var area = SKRect.Create(0, 0, width, hight);
+                renderer.Render(canvas, area, data);
+            }
+        }
+
+        public static void Render(this SKCanvas canvas, QRCodeData data, SKRect area, SKColor clearColor)
+        {
+            canvas.Clear(clearColor);
+
+            using (var renderer = new QRCodeRenderer())
+            {
+                renderer.Render(canvas, area, data);
+            }
+        }
+    }
+}
