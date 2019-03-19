56771a3c9a643647892e60e64c8d974b47461085
guitarrapc <3856350+guitarrapc@users.noreply.github.com>
2019-03-19 15:32:21 +0900

    feat: add simple generate QR Code sample

---
+++ sample/SimpleGenerate/Program.cs
@@ -0,0 +1,27 @@
+using SkiaSharp;
+using SkiaSharp.QrCode.Image;
+using System;
+using System.IO;
+
+namespace SimpleGenerate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: dotnet SimpleGenerate.dll your_message output_path");
+                return;
+            }
+            var content = args[0];
+            var path = args[1];
+            var qrCode = new QrCode(content, new Vector2Slim(256, 256), SKEncodedImageFormat.Png);
+            using (var output = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                qrCode.GenerateImage(output);
+            }
+            Console.WriteLine($"Successfully output QRCode in {path}");
+        }
+    }
+}
