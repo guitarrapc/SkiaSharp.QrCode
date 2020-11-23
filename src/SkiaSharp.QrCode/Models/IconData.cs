9ff473d42963898d173ec92d9643b140afe80e97
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2020-11-24 05:51:13 +0900

    feat: default icon size 15 is too big.

--- src/SkiaSharp.QrCode/Models/IconData.cs
+++ src/SkiaSharp.QrCode/Models/IconData.cs
@@ -7,6 +7,6 @@ namespace SkiaSharp.QrCode.Models
     public class IconData
     {
         public SKBitmap Icon { get; set; }
-        public int IconSizePercent { get; set; } = 15;
+        public int IconSizePercent { get; set; } = 10;
     }
 }
