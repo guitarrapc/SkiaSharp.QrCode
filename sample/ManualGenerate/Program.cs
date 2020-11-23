aa5f47095a3c100fddc292449f374cdc0ed52d33
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2020-11-24 05:52:47 +0900

    chore: add Color and Icon samples

--- sample/ManualGenerate/Program.cs
+++ sample/ManualGenerate/Program.cs
@@ -1,7 +1,8 @@
-﻿using SkiaSharp;
+using SkiaSharp;
 using System;
 using System.IO;
 using SkiaSharp.QrCode;
+using SkiaSharp.QrCode.Models;
 
 namespace SkiaQrCodeSampleConsole
 {
@@ -9,6 +10,8 @@ namespace SkiaQrCodeSampleConsole
     {
         static void Main(string[] args)
         {
+            Directory.CreateDirectory("output");
+
             var content = "testtesttest";
             using (var generator = new QRCodeGenerator())
             {
@@ -21,6 +24,21 @@ namespace SkiaQrCodeSampleConsole
                 {
                     var canvas = surface.Canvas;
                     canvas.Render(qr, info.Width, info.Height);
+
+                    // gen color
+                    // yellow https://rgb.to/yellow
+                    //canvas.Render(qr, info.Width, info.Height, SKColor.Empty, SKColor.FromHsl(60,100,50));
+                    // red https://rgb.to/red
+                    //canvas.Render(qr, info.Width, info.Height, SKColor.Empty, SKColor.FromHsl(0, 100, 50));
+
+                    // gen icon
+                    //var logo = File.ReadAllBytes("samples/test.png");
+                    //var icon = new IconData
+                    //{
+                    //    Icon = SKBitmap.Decode(logo),
+                    //    IconSizePercent = 10,
+                    //};
+                    //canvas.Render(qr, info.Width, info.Height, SKColor.Empty, SKColor.Parse("000000"), icon);
 
                     // Output to Stream -> File
                     using (var image = surface.Snapshot())
