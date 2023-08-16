c89c1554d405d8624e914015ce3033896f4c6983
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2023-08-16 18:59:07 +0900

    chore: Sample BlazorWasm update styling.

--- samples/BlazorWasm/Program.cs
+++ samples/BlazorWasm/Program.cs
@@ -1,10 +1,12 @@
 using BlazorWasm;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Fast.Components.FluentUI;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddFluentUIComponents(); // add Fluent UI
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
