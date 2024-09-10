b3df8a6e99a8938697f725f11f4da92cd466f926
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2024-09-10 09:20:58 +0900

    refactor: use full qualified name

--- samples/BlazorWasm/Program.cs
+++ samples/BlazorWasm/Program.cs
@@ -1,10 +1,9 @@
-using BlazorWasm;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Fast.Components.FluentUI;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<BlazorWasm.App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddFluentUIComponents(); // add Fluent UI
 
