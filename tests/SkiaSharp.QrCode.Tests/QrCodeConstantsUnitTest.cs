93a1a6a66013b172775cf893952fc4e42a2e1654
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2025-10-03 09:29:54 +0900

    chore: add Test for AlphanumEncTable

--- tests/SkiaSharp.QrCode.Tests/QrCodeConstantsUnitTest.cs
+++ tests/SkiaSharp.QrCode.Tests/QrCodeConstantsUnitTest.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Xunit;
 using static SkiaSharp.QrCode.QRCodeGenerator;
 
@@ -21,6 +22,30 @@ public class QrCodeConstantsUnitTest
     public void IsNumericTest(char c, bool expected)
     {
         var result = QrCodeConstants.IsNumeric(c);
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData('0', true)]
+    [InlineData('9', true)]
+    [InlineData('A', true)]
+    [InlineData('Z', true)]
+    [InlineData(' ', true)]
+    [InlineData('$', true)]
+    [InlineData('%', true)]
+    [InlineData('*', true)]
+    [InlineData('+', true)]
+    [InlineData('-', true)]
+    [InlineData('.', true)]
+    [InlineData('/', true)]
+    [InlineData(':', true)]
+    [InlineData('a', false)] // 小文字は無効
+    [InlineData('z', false)]
+    [InlineData('@', false)]
+    [InlineData('#', false)]
+    public void AlphanumEncTable(char c, bool expected)
+    {
+        var result = QrCodeConstants.AlphanumEncTable.Contains(c);
         Assert.Equal(expected, result);
     }
 }
