eb7a8d2eaaf1acf93e0455f311ac94ab9caa31b9
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2025-10-14 03:36:23 +0900

    chore: add comment

--- src/SkiaSharp.QrCode/QRCodeExtensions.cs
+++ src/SkiaSharp.QrCode/QRCodeExtensions.cs
@@ -4,6 +4,13 @@ namespace SkiaSharp.QrCode;
 
 public static class QRCodeExtensions
 {
+    /// <summary>
+    /// Renders a QR code on the canvas with default colors.
+    /// </summary>
+    /// <param name="canvas">The canvas to render on.</param>
+    /// <param name="data">The QR code data.</param>
+    /// <param name="width">The width of the rendering area.</param>
+    /// <param name="hight">The height of the rendering area.</param>
     public static void Render(this SKCanvas canvas, QRCodeData data, int width, int hight)
     {
         canvas.Clear(SKColors.Transparent);
@@ -12,6 +19,15 @@ public static class QRCodeExtensions
         QRCodeRenderer.Render(canvas, area, data, null, null, null);
     }
 
+    /// <summary>
+    /// Renders a QR code on the canvas with custom colors.
+    /// </summary>
+    /// <param name="canvas">The canvas to render on.</param>
+    /// <param name="data">The QR code data.</param>
+    /// <param name="width">The width of the rendering area.</param>
+    /// <param name="hight">The height of the rendering area.</param>
+    /// <param name="clearColor">The color used to clear the canvas.</param>
+    /// <param name="codeColor">The color of the QR code modules.</param>
     public static void Render(this SKCanvas canvas, QRCodeData data, int width, int hight, SKColor clearColor, SKColor codeColor)
     {
         canvas.Clear(clearColor);
@@ -20,6 +36,16 @@ public static class QRCodeExtensions
         QRCodeRenderer.Render(canvas, area, data, codeColor, null);
     }
 
+    /// <summary>
+    /// Renders a QR code on the canvas with custom colors.
+    /// </summary>
+    /// <param name="canvas">The canvas to render on.</param>
+    /// <param name="data">The QR code data.</param>
+    /// <param name="width">The width of the rendering area.</param>
+    /// <param name="hight">The height of the rendering area.</param>
+    /// <param name="clearColor">The color used to clear the canvas.</param>
+    /// <param name="codeColor">The color of the QR code modules.</param>
+    /// <param name="backgroundColor">The background color of the QR Code canvas.</param>
     public static void Render(this SKCanvas canvas, QRCodeData data, int width, int hight, SKColor clearColor, SKColor codeColor, SKColor backgroundColor)
     {
         canvas.Clear(clearColor);
@@ -28,6 +54,16 @@ public static class QRCodeExtensions
         QRCodeRenderer.Render(canvas, area, data, codeColor, backgroundColor);
     }
 
+    /// <summary>
+    /// Renders a QR code on the canvas with custom colors.
+    /// </summary>
+    /// <param name="canvas">The canvas to render on.</param>
+    /// <param name="data">The QR code data.</param>
+    /// <param name="width">The width of the rendering area.</param>
+    /// <param name="hight">The height of the rendering area.</param>
+    /// <param name="clearColor">The color used to clear the canvas.</param>
+    /// <param name="codeColor">The color of the QR code modules.</param>
+    /// <param name="iconData">Optional icon data to overlay on the center of the QR code.</param>
     public static void Render(this SKCanvas canvas, QRCodeData data, int width, int hight, SKColor clearColor, SKColor codeColor, IconData iconData)
     {
         canvas.Clear(clearColor);
@@ -36,6 +72,17 @@ public static class QRCodeExtensions
         QRCodeRenderer.Render(canvas, area, data, codeColor, null, iconData);
     }
 
+    /// <summary>
+    /// Renders a QR code on the canvas with custom colors.
+    /// </summary>
+    /// <param name="canvas">The canvas to render on.</param>
+    /// <param name="data">The QR code data.</param>
+    /// <param name="width">The width of the rendering area.</param>
+    /// <param name="hight">The height of the rendering area.</param>
+    /// <param name="clearColor">The color used to clear the canvas.</param>
+    /// <param name="codeColor">The color of the QR code modules.</param>
+    /// <param name="backgroundColor">The background color of the QR Code canvas.</param>
+    /// <param name="iconData">Optional icon data to overlay on the center of the QR code.</param>
     public static void Render(this SKCanvas canvas, QRCodeData data, int width, int hight, SKColor clearColor, SKColor codeColor, SKColor backgroundColor, IconData iconData)
     {
         canvas.Clear(clearColor);
@@ -44,6 +91,14 @@ public static class QRCodeExtensions
         QRCodeRenderer.Render(canvas, area, data, codeColor, backgroundColor, iconData);
     }
 
+    /// <summary>
+    /// Renders a QR code on the canvas with custom colors.
+    /// </summary>
+    /// <param name="canvas">The canvas to render on.</param>
+    /// <param name="data">The QR code data.</param>
+    /// <param name="area">The rectangular area where the QR code will be rendered.</param>
+    /// <param name="clearColor">The color used to clear the canvas.</param>
+    /// <param name="codeColor">The color of the QR code modules.</param>
     public static void Render(this SKCanvas canvas, QRCodeData data, SKRect area, SKColor clearColor, SKColor codeColor)
     {
         canvas.Clear(clearColor);
@@ -51,6 +106,15 @@ public static class QRCodeExtensions
         QRCodeRenderer.Render(canvas, area, data, codeColor, null);
     }
 
+    /// <summary>
+    /// Renders a QR code on the canvas with custom colors.
+    /// </summary>
+    /// <param name="canvas">The canvas to render on.</param>
+    /// <param name="data">The QR code data.</param>
+    /// <param name="area">The rectangular area where the QR code will be rendered.</param>
+    /// <param name="clearColor">The color used to clear the canvas.</param>
+    /// <param name="codeColor">The color of the QR code modules.</param>
+    /// <param name="backgroundColor">The background color of the QR Code canvas.</param>
     public static void Render(this SKCanvas canvas, QRCodeData data, SKRect area, SKColor clearColor, SKColor codeColor, SKColor backgroundColor)
     {
         canvas.Clear(clearColor);
@@ -58,6 +122,15 @@ public static class QRCodeExtensions
         QRCodeRenderer.Render(canvas, area, data, codeColor, backgroundColor);
     }
 
+    /// <summary>
+    /// Renders a QR code on the canvas with custom colors.
+    /// </summary>
+    /// <param name="canvas">The canvas to render on.</param>
+    /// <param name="data">The QR code data.</param>
+    /// <param name="area">The rectangular area where the QR code will be rendered.</param>
+    /// <param name="clearColor">The color used to clear the canvas.</param>
+    /// <param name="codeColor">The color of the QR code modules.</param>
+    /// <param name="iconData">Optional icon data to overlay on the center of the QR code.</param>
     public static void Render(this SKCanvas canvas, QRCodeData data, SKRect area, SKColor clearColor, SKColor codeColor, IconData iconData)
     {
         canvas.Clear(clearColor);
@@ -65,6 +138,16 @@ public static class QRCodeExtensions
         QRCodeRenderer.Render(canvas, area, data, codeColor, null, iconData);
     }
 
+    /// <summary>
+    /// Renders a QR code on the canvas with custom colors.
+    /// </summary>
+    /// <param name="canvas">The canvas to render on.</param>
+    /// <param name="data">The QR code data.</param>
+    /// <param name="area">The rectangular area where the QR code will be rendered.</param>
+    /// <param name="clearColor">The color used to clear the canvas.</param>
+    /// <param name="codeColor">The color of the QR code modules.</param>
+    /// <param name="backgroundColor">The background color of the QR Code canvas.</param>
+    /// <param name="iconData">Optional icon data to overlay on the center of the QR code.</param>
     public static void Render(this SKCanvas canvas, QRCodeData data, SKRect area, SKColor clearColor, SKColor codeColor, SKColor backgroundColor, IconData iconData)
     {
         canvas.Clear(clearColor);
--- src/SkiaSharp.QrCode/QRCodeRenderer.cs
+++ src/SkiaSharp.QrCode/QRCodeRenderer.cs
@@ -7,11 +7,13 @@ public static class QRCodeRenderer
     /// <summary>
     /// Render the specified data into the given area of the target canvas.
     /// </summary>
-    /// <param name="canvas">The canvas.</param>
-    /// <param name="area">The area.</param>
-    /// <param name="data">The data.</param>
-    /// <param name="qrColor">The color.</param>
-    /// <param name="iconData">The icon settings</param>
+    /// <param name="canvas">The canvas to render the QR code on.</param>
+    /// <param name="area">The rectangular area where the QR code will be rendered.</param>
+    /// <param name="data">The QR code data to render.</param>
+    /// <param name="qrColor">The color of the QR code modules. If null, black is used.</param>
+    /// <param name="backgroundColor">The background color. If null, white is used.</param>
+    /// <param name="iconData">Optional icon data to overlay on the center of the QR code.</param>
+    /// <exception cref="ArgumentNullException"></exception>
     public static void Render(SKCanvas canvas, SKRect area, QRCodeData data, SKColor? qrColor, SKColor? backgroundColor, IconData? iconData = null)
     {
         if (data is null)
