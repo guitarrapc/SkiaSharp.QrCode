fb20ee089a24c0020a80142307bb010911fe2bf5
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2025-10-17 04:12:37 +0900

    chore: change generation

--- samples/Benchmark/QrGeneratorSimple.cs
+++ samples/Benchmark/QrGeneratorSimple.cs
@@ -4,24 +4,19 @@ public class QrGeneratorSimple
     public IEnumerable<object[]> ByteTextDataSource()
     {
         ECCLevel[] eccLevels = [ECCLevel.L, ECCLevel.M, ECCLevel.Q];
-        foreach (var ecc in eccLevels)
-        {
-            yield return ["https://exmaple.com/foobar", ecc];
-        }
+        string[] texts = [
+            "https://exmaple.com/foobar",
+            "Medium length text with some special characters !@#$%",
+            "Very long text that will definitely require multiple string concatenations and cause significant memory allocations during the QR code generation process",
+            "Zorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et nunc placerat, pellentesque nisi volutpat, rhoncus massa. Pellentesque pellentesque, mi ut rutrum tincidunt, nisl felis rhoncus nisi, eu ultrices odio nulla finibus diam. Nam quis velit leo. Morbi ac tortor justo. Maecenas in lectus purus. Vestibulum varius porta congue. Nulla facilisi. Mauris feugiat tincidunt metus, vel dictum ex fermentum eget.             Aenean convallis ut libero nec laoreet. Pellentesque vel mi id odio dapibus aliquet quis ac dolor. Nunc molestie lacinia diam, vitae tincidunt nunc. Donec varius ornare lorem ac dictum. Integer at posuere neque, ut accumsan dui. Fusce nec nisi accumsan, aliquam justo ac, venenatis ex. Sed eget nunc diam. Pellentesque quis arcu volutpat, facilisis sapien vitae, pharetra tellus. Sed gravida lacus sed lacus consequat porttitor. Nam a orci interdum, sollicitudin enim id, facilisis nisl. Donec ultrices fringilla tempus. Nullam vestibulum faucibus ullamcorper est.",
+        ];
 
         foreach (var ecc in eccLevels)
         {
-            yield return ["Medium length text with some special characters !@#$%", ecc];
-        }
-
-        foreach (var ecc in eccLevels)
-        {
-            yield return ["Very long text that will definitely require multiple string concatenations and cause significant memory allocations during the QR code generation process", ecc];
-        }
-
-        foreach (var ecc in eccLevels)
-        {
-            yield return ["Zorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et nunc placerat, pellentesque nisi volutpat, rhoncus massa. Pellentesque pellentesque, mi ut rutrum tincidunt, nisl felis rhoncus nisi, eu ultrices odio nulla finibus diam. Nam quis velit leo. Morbi ac tortor justo. Maecenas in lectus purus. Vestibulum varius porta congue. Nulla facilisi. Mauris feugiat tincidunt metus, vel dictum ex fermentum eget.             Aenean convallis ut libero nec laoreet. Pellentesque vel mi id odio dapibus aliquet quis ac dolor. Nunc molestie lacinia diam, vitae tincidunt nunc. Donec varius ornare lorem ac dictum. Integer at posuere neque, ut accumsan dui. Fusce nec nisi accumsan, aliquam justo ac, venenatis ex. Sed eget nunc diam. Pellentesque quis arcu volutpat, facilisis sapien vitae, pharetra tellus. Sed gravida lacus sed lacus consequat porttitor. Nam a orci interdum, sollicitudin enim id, facilisis nisl. Donec ultrices fringilla tempus. Nullam vestibulum faucibus ullamcorper est.", ecc];
+            foreach (var text in texts)
+            {
+                yield return new object[] { text, ecc };
+            }
         }
     }
 
