38a2c084a14f65086ef40dcf3eaff0b0edff2661
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2025-10-20 02:42:51 +0900

    chore: update benchmark

--- samples/Benchmark/SimpleEncode.cs
+++ samples/Benchmark/SimpleEncode.cs
@@ -1,18 +1,26 @@
+using BenchmarkDotNet.Configs;
 using SkiaSharp;
 
+[GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory)]
 [MemoryDiagnoser]
 public class SimpleEncode
 {
+    private string _textNumber = default!;
+    private string _textAlphanumeric = default!;
     private string _textUrl = default!;
     private string _textUnicode = default!;
+    private string _textWifi = default!;
     ZXing.BarcodeWriter<SKBitmap> _zxingWriter_ascii = default!;
     ZXing.BarcodeWriter<SKBitmap> _zxingWriter_utf8 = default!;
 
     [GlobalSetup]
     public void GlobalSetup()
     {
+        _textNumber = "0123456789012345678901234567890123456789";
+        _textAlphanumeric = "0123456789ABCDEFG0123456789HIJKLMN";
         _textUrl = "https://example.com/user/repo?foo=value&bar=piyo";
-        _textUnicode = "你好世界こんにちはПривет мир🎉🎊🎈Zürich";
+        _textUnicode = "FooBar你好世界こんにちはПривет мир🎉🎊🎈Zürich";
+        _textWifi = "WIFI:S:foobar-wifi;T:WPA;P:test123;H:false;;";
         _zxingWriter_ascii = new()
         {
             Format = ZXing.BarcodeFormat.QR_CODE,
@@ -34,38 +42,107 @@ public class SimpleEncode
     }
 
     [Benchmark(Baseline = true)]
+    [BenchmarkCategory("SkiaSharp.QrCode")]
+    public QRCodeData SkiaSharpQRCode_Number_Encode()
+    {
+        return SkiaSharp.QrCode.QRCodeGenerator.CreateQrCode(_textNumber.AsSpan(), ECCLevel.L);
+    }
+
+    [Benchmark(Baseline = true)]
+    [BenchmarkCategory("QRCoder")]
+    public QRCoder.QRCodeData QRCoder_Number_Encode()
+    {
+        return QRCoder.QRCodeGenerator.GenerateQrCode(_textNumber, QRCoder.QRCodeGenerator.ECCLevel.L);
+    }
+
+    [Benchmark(Baseline = true)]
+    [BenchmarkCategory("Zxing")]
+    public ZXing.Common.BitMatrix ZXing_Number_Encode()
+    {
+        return _zxingWriter_ascii.Encode(_textNumber);
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("SkiaSharp.QrCode")]
+    public QRCodeData SkiaSharpQRCode_Alphanumeric_Encode()
+    {
+        return SkiaSharp.QrCode.QRCodeGenerator.CreateQrCode(_textAlphanumeric.AsSpan(), ECCLevel.L);
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("QRCoder")]
+    public QRCoder.QRCodeData QRCoder_Alphanumeric_Encode()
+    {
+        return QRCoder.QRCodeGenerator.GenerateQrCode(_textAlphanumeric, QRCoder.QRCodeGenerator.ECCLevel.L);
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("Zxing")]
+    public ZXing.Common.BitMatrix ZXing_Alphanumeric_Encode()
+    {
+        return _zxingWriter_ascii.Encode(_textAlphanumeric);
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("SkiaSharp.QrCode")]
     public QRCodeData SkiaSharpQRCode_Url_Encode()
     {
         return SkiaSharp.QrCode.QRCodeGenerator.CreateQrCode(_textUrl.AsSpan(), ECCLevel.L);
     }
 
     [Benchmark]
+    [BenchmarkCategory("QRCoder")]
     public QRCoder.QRCodeData QRCoder_Url_Encode()
     {
         return QRCoder.QRCodeGenerator.GenerateQrCode(_textUrl, QRCoder.QRCodeGenerator.ECCLevel.L);
     }
 
     [Benchmark]
+    [BenchmarkCategory("Zxing")]
     public ZXing.Common.BitMatrix ZXing_Url_Encode()
     {
         return _zxingWriter_ascii.Encode(_textUrl);
     }
 
     [Benchmark]
+    [BenchmarkCategory("SkiaSharp.QrCode")]
     public QRCodeData SkiaSharpQRCode_Unicode_Encode()
     {
         return SkiaSharp.QrCode.QRCodeGenerator.CreateQrCode(_textUnicode.AsSpan(), ECCLevel.L);
     }
 
     [Benchmark]
+    [BenchmarkCategory("QRCoder")]
     public QRCoder.QRCodeData QRCoder_Unicode_Encode()
     {
         return QRCoder.QRCodeGenerator.GenerateQrCode(_textUnicode, QRCoder.QRCodeGenerator.ECCLevel.L);
     }
 
     [Benchmark]
+    [BenchmarkCategory("Zxing")]
     public ZXing.Common.BitMatrix ZXing_Unicode_Encode()
     {
         return _zxingWriter_utf8.Encode(_textUnicode);
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("SkiaSharp.QrCode")]
+    public QRCodeData SkiaSharpQRCode_Wifi_Encode()
+    {
+        return SkiaSharp.QrCode.QRCodeGenerator.CreateQrCode(_textWifi.AsSpan(), ECCLevel.L);
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("QRCoder")]
+    public QRCoder.QRCodeData QRCoder_Wifi_Encode()
+    {
+        return QRCoder.QRCodeGenerator.GenerateQrCode(_textWifi, QRCoder.QRCodeGenerator.ECCLevel.L);
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("Zxing")]
+    public ZXing.Common.BitMatrix ZXing_Wifi_Encode()
+    {
+        return _zxingWriter_utf8.Encode(_textWifi);
     }
 }
