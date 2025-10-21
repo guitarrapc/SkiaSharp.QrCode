f1942781d6f7a43758ce91f3f00df0f734d9e150
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2025-10-22 01:51:42 +0900

    chore: benchmark for serialize/deserialize

---
+++ samples/Benchmark/SrializeSimple.cs
@@ -0,0 +1,37 @@
+[MemoryDiagnoser]
+public class SrializeSimple
+{
+    private QRCodeData _qrCode = default!;
+
+    public IEnumerable<object[]> ByteTextDataSource()
+    {
+        var compressions = new Compression[] { Compression.Uncompressed, Compression.Deflate, Compression.GZip };
+        var qrCode = QRCodeGenerator.CreateQrCode("https://exmaple.com/foobar", ECCLevel.L);
+
+        foreach (var compression in compressions)
+        {
+            yield return new object[] { qrCode.GetRawData(compressMode: compression), compression };
+        }
+    }
+
+    public SrializeSimple()
+    {
+        _qrCode = QRCodeGenerator.CreateQrCode("https://exmaple.com/foobar", ECCLevel.L);
+    }
+
+    [Benchmark]
+    [Arguments(Compression.Uncompressed)]
+    [Arguments(Compression.Deflate)]
+    [Arguments(Compression.GZip)]
+    public void Serialize(Compression compression)
+    {
+        _qrCode.GetRawData(compression);
+    }
+
+    [Benchmark]
+    [ArgumentsSource(nameof(ByteTextDataSource))]
+    public void Deerialize(byte[] rawData, Compression compression)
+    {
+        _ = new QRCodeData(rawData, compression, quietZoneSize: 0);
+    }
+}
