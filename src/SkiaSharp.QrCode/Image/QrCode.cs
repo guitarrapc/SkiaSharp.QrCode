86f63aa5ef6ff1e8d0fd49c1c2e1b2a08bb2b9be
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2025-10-25 01:51:27 +0900

    feat: Obsolete QrCode class

--- src/SkiaSharp.QrCode/Image/QrCode.cs
+++ src/SkiaSharp.QrCode/Image/QrCode.cs
@@ -1,7 +1,11 @@
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 
 namespace SkiaSharp.QrCode.Image;
 
+/// <summary>
+/// QR code generator (Obsolete - use <see cref="QRCodeBuilder"/> instead).
+/// </summary>
+[Obsolete("This class is obsolete. Use QRCodeBuilder for more flexible and feature-rich QR code generation.", false)]
 public class QrCode
 {
     private readonly string _content;
