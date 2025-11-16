a9adb7e381e1131659c4fe45be31d54566d9a7cc
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2025-11-16 19:39:25 +0900

    feat(Breaking change): Add Iconshape to handle QR Icon.
    Legacy SKBitmap now use IconData.FromImage instead.

--- src/SkiaSharp.QrCode/Image/IconData.cs
+++ src/SkiaSharp.QrCode/Image/IconData.cs
@@ -3,9 +3,9 @@ namespace SkiaSharp.QrCode.Image;
 public class IconData
 {
     /// <summary>
-    /// The icon bitmap to overlay on the QR code.
+    /// The icon shape to overlay on the QR code.
     /// </summary>
-    public SKBitmap? Icon { get; set; }
+    public required IconShape Icon { get; set; }
     /// <summary>
     /// The size of the icon as a percentage of the QR code size (1-100).
     /// </summary>
@@ -14,4 +14,21 @@ public class IconData
     /// The border width around the icon in pixels. Creates a background-colored padding around the icon.
     /// </summary>
     public int IconBorderWidth { get; set; } = 2;
+
+    /// <summary>
+    /// Create IconData from a bitmap image
+    /// </summary>
+    /// <param name="image"></param>
+    /// <param name="iconSizePercent"></param>
+    /// <param name="iconBorderWidth"></param>
+    /// <returns></returns>
+    public static IconData FromImage(SKBitmap image, int iconSizePercent = 10, int iconBorderWidth = 2)
+    {
+        return new IconData
+        {
+            Icon = new ImageIconShape(image),
+            IconSizePercent = iconSizePercent,
+            IconBorderWidth = iconBorderWidth
+        };
+    }
 }
